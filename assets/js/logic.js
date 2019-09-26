5ebbbf16d4491fdb71337abea859ed3a95314288
Juan Villela <juan@villela.co>
2019-09-26 13:38:05 -0400

    Session input updates
    Removed mouse movement input.
    
    Added circle click/tap listener for reset and pause.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,82 +1,54 @@
-// Gyronorm
-const args = {
-  decimalCount: 1,
-  frequency: 60,
-  gravityNormalized: true,
-  logger: null,
-  screenAdjusted: false,
-};
-const gn = new GyroNorm();
+// Start session timer with a little delay to wait for fade in animation
+setTimeout(() => {
+  startTimer();
+  startInstructions();
+}, 600);
 
-// Mobile reset
-const checkMotion = () => {
-  gn.init(args).then(() => {
-    gn.start(data => {
-      const x = data.dm.x;
-      const y = data.dm.y;
+// Reset session on circle click within the first 10s
+// Pause session on circle click after 10s
+circle.addEventListener('click', () => {
+  if (timer < 10) {
+    if (!isPaused) {
+      resetTimer();
+      resetInstructions();
+      resetHints();
+    }
+  } else if (timer > 10) {
+    isPaused = !isPaused;
+  }
+});
 
-      circle.innerHTML = `${x}, ${y}`;
-
-      if (x !== 0 && timer >= 0 && timer <= 10) {
-        isPaused = !isPaused;
-      }
-    });
-  });
-};
-
-// Grand Permission
-const grandPremission = () => {
-  DeviceOrientationEvent.requestPermission()
-    .then(permissionState => {
-      if (permissionState === 'granted') {
-        checkMotion();
-        audio.volume = vol;
-      }
-    })
-    .catch(console.error);
-};
+// Reset session on tab change within the first 10s
+document.addEventListener('visibilitychange', () => {
+  if (timer < 10) {
+    if (!isPaused) {
+      resetTimer();
+      resetInstructions();
+      resetHints();
+    }
+  }
+});
 
 // Check OS
 if (
   navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)
 ) {
+  // Inset desktop hints
   hintW1.innerText = welcome.w1.motion;
   med1.innerText = meditation.m1;
   med2.innerText = meditation.m2;
   end1.innerText = end.m1;
   end2.innerText = end.m2;
-
-  setTimeout(() => {
-    startTimer();
-    startInstructions();
-  }, 600);
-
-  checkMotion();
 } else {
+  // Inset mobile hints
   hintW1.innerText = welcome.w1.desktop;
   med1.innerText = meditation.d1;
   med2.innerText = meditation.d2;
   end1.innerText = end.d1;
   end2.innerText = end.d2;
 
-  setTimeout(() => {
-    startTimer();
-    startInstructions();
-  }, 600);
-
-  // Desktop reset
-  document.addEventListener('mousemove', () => {
-    if (timer < 10) {
-      if (!isPaused) {
-        resetTimer();
-        resetInstructions();
-        resetHints();
-      }
-    } else if (timer >= 0 && timer <= 10) {
-      isPaused = !isPaused;
-    }
-  });
-
+  // Reset session with SPACE
+  // Pause session with START
   document.addEventListener('keydown', e => {
     if (e.keyCode === 13) {
       resetTimer();
@@ -84,18 +56,6 @@ if (
       resetHints();
       isPaused = false;
     } else if (e.keyCode === 32) {
-      isPaused = !isPaused;
-    }
-  });
-
-  document.addEventListener('visibilitychange', () => {
-    if (timer < 10) {
-      if (!isPaused) {
-        resetTimer();
-        resetInstructions();
-        resetHints();
-      }
-    } else if (timer >= 0 && timer <= 10) {
       isPaused = !isPaused;
     }
   });
